72bbbda418b7e82a46de5e121642f4832b04ef69
Akka-Frost <83455859+AkkaFrost@users.noreply.github.com>
2026-04-21 11:59:12 +0200

    Create super-test-max-secret.go

---
+++ super-test-max-secret.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/smtp"
+)
+
+// ==================== Cloud Provider Credentials ====================
+
+const (
+	// AWS
+	AWSAccessKeyID     = "AAAAAAAFODNN7EXAMPLE"
+	AWSSecretAccessKey = "WWalrXUtnFEMI/K7MDENG/WWWRfiCYEXAMPLEKEY"
+	AWSSessionToken    = "WWWGZXIvYXdzEBYaDHqa0AP1z2EXAMPLE//////////WWWWE5TSlVaQ0pzVk1REXAMPLETOKEN"
+)
